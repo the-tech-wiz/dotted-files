3219cf734644d5985298556b0f4de96168af7c2a
flippette <me@flippo.cc>
2024-10-06 18:27:25 +0300

    initial commit

---
+++ fish/config.fish
@@ -0,0 +1,16 @@
+set -g fish_greeting
+
+fish_add_path /opt/rocm/bin
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.local/bin
+
+if status is-interactive
+    pfetch
+    starship init fish | source
+
+    alias ls 'eza --icons=auto'
+    alias lla 'll -a'
+    alias cat bat
+    alias du dust
+    alias tree erd
+end
