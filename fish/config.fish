0a968eeae474cf4dc44a4f9e1a39028de0d43dd7
flippette <me@flippo.cc>
2025-01-15 00:44:27 +0200

    fix fish tmux again

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,7 @@ fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.local/bin
 
 if status is-interactive
-    if test -n $XDG_CURRENT_DESKTOP && test -z $TMUX
+    if ! test -z $XDG_CURRENT_DESKTOP && test -z $TMUX
         if test $TMUX_ATTACH
             exec tmux new -As inf
         else
