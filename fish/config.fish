12023038ab5c53fa68597f07647f18d069fbabae
flippette <me@flippo.cc>
2025-01-14 11:23:01 +0200

    ...stuff

--- fish/config.fish
+++ fish/config.fish
@@ -5,8 +5,8 @@ fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.local/bin
 
 if status is-interactive
-    if ! test -z $XDG_CURRENT_DESKTOP && test -z $TMUX
-        if $TMUX_ATTACH
+    if test -n $XDG_CURRENT_DESKTOP && test -z $TMUX
+        if test $TMUX_ATTACH
             exec tmux new -As inf
         else
             exec tmux new
