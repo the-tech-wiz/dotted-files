ddbae0a72d26b7df1c1370731ba0af71962c32b8
the-tech-wiz <thetechwizard@proton.me>
2025-09-21 01:22:21 +0300

    rejigger fish, added zoxide, nix, replaced delta for difftastic

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,33 @@
 set -g fish_greeting
 
+# disable direnv warning?
+set -gx DIRENV_WARN_TIMEOUT 0
+
+# use `difftastic` for git diff
+set -gx GIT_EXTERNAL_DIFF difft
+
+# XDG base dirs
+set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_STATE_HOME $HOME/.local/state
+set -gx XDG_CACHE_HOME $HOME/.cache
+
+# rust home dirs
+set -gx RUSTUP_HOME $XDG_DATA_HOME/rustup
+set -gx CARGO_HOME $XDG_DATA_HOME/cargo
+
+#nix
+source "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish"
+fish_add_path -gP $XDG_STATE_HOME/nix/profile/bin
+
+fish_add_path $HOME/.cargo/bin
 fish_add_path /opt/rocm/bin
 fish_add_path /opt/cuda/bin
-fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.local/share/coursier/bin
 
+#interactive
 if status is-interactive
-    pfetch
     starship init fish | source
     direnv hook fish | source
 
@@ -17,4 +37,7 @@ if status is-interactive
     alias du dust
     alias tree erd
     alias rq 'dnf repoquery'
+    pfetch
 end
+
+zoxide init fish | source
